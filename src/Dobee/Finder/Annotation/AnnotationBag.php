9cf99f13836c04f337567d73d9d4cea01d64fc3d
janhuang <384099566@qq.com>
2015-03-07 11:42:06 +0800

    fix annotation reflection bug

--- src/Dobee/Finder/Annotation/AnnotationBag.php
+++ src/Dobee/Finder/Annotation/AnnotationBag.php
@@ -109,12 +109,8 @@ class AnnotationBag implements AnnotationBagInterface, AnnotationParserInterface
                         if (isset($mapped['method']) && !empty($mapped['method'])) {
                             $parameters = $reflectionClass->getMethod($mapped['method'])->getParameters();
                             foreach ($parameters as $index => $param) {
-                                $name = is_object($param->getClass()) ?
-                                    function () use ($param) {
-                                        return $param->getClass()->getName();
-                                    }
-                                    : $param->getName();
-                                $parameters[$index] = $name;
+                                if (is_object($param->getClass())) { continue; }
+                                $parameters[$index] = $param->getName();
                             }
                             $mapped['parameters'] = $parameters;
                         }
